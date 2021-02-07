51148e5a0ba9f204480c22e6841e03b3a16fe3ec
chairielazizi <azizihr2000@gmail.com>
2021-02-07 21:17:41 +0800

    setel the addview basic fx

--- lib/ui/addview.dart
+++ lib/ui/addview.dart
@@ -21,6 +21,7 @@ class _AddViewState extends State<AddView> {
   Widget build(BuildContext context) {
     return Material(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           DropdownButton(
             value: dropdownvalue,
@@ -29,7 +30,37 @@ class _AddViewState extends State<AddView> {
                 dropdownvalue = value;
               });
             },
-            items: coins.map<DropdownMenuItem<String>>((String value) {}),
+            items: coins.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.3,
+            child: TextFormField(
+              controller: _amountController,
+              decoration: InputDecoration(
+                labelText: "Coin Amount",
+              ),
+              keyboardType: TextInputType.text,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.4,
+            height: 45,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: Colors.white,
+            ),
+            child: MaterialButton(
+              onPressed: () async {
+                // call firestore method for adding
+                Navigator.of(context).pop();
+              },
+              child: Text("Add"),
+            ),
           ),
         ],
       ),
