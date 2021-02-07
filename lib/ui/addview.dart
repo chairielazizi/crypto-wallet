5cb350560af69bea742c75e8618e446e2f6f6e7f
chairielazizi <azizihr2000@gmail.com>
2021-02-07 21:02:28 +0800

    add dropdown menu to addview fx

--- lib/ui/addview.dart
+++ lib/ui/addview.dart
@@ -8,10 +8,31 @@ class AddView extends StatefulWidget {
 }
 
 class _AddViewState extends State<AddView> {
+  // dropdown value
+  List<String> coins = [
+    "bitcoin",
+    "tether",
+    "ethereum",
+  ];
+  String dropdownvalue = "bitcoin";
+  TextEditingController _amountController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Text("Add View"),
+      child: Column(
+        children: [
+          DropdownButton(
+            value: dropdownvalue,
+            onChanged: (String value) {
+              setState(() {
+                dropdownvalue = value;
+              });
+            },
+            items: coins.map<DropdownMenuItem<String>>((String value) {}),
+          ),
+        ],
+      ),
     );
   }
 }
